7ac5a53f27b1d20a77a9951764a0719c782439e8
kenser <koffy140221@gmail.com>
2023-07-14 10:59:40 -0600

    CREAR FORMULARIO

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONSULTA PHP</title>
+</head>
+<body>
+    <h1>API FETCH</h1>
+    <h2>CONSULTA UN ARCHIVO PHP</h2>
+    <form>
+        <input type="text" name="pokemon" id="pokemon">
+        <button type="submit">Consultar</button>
+        <button type="submit">Consultar</button> 
+        <button id="buttonConsulta" type="button">Consultar a PHP</button>
+    </form>
+    <script src="script.js"></script>
+</body>
+</html>
