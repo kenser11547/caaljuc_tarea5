26439a710cc12fb93a367af58c94506f6834b4e5
kenser <koffy140221@gmail.com>
2023-07-14 11:11:04 -0600

    crear formulario con html

--- formulario.php
+++ formulario.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONSULTA PHP</title>
+    <title>CONSULTA CON PHP</title>
 </head>
 <body>
-    <h1>API FETCH</h1>
-    <h2>CONSULTA UN ARCHIVO PHP</h2>
-    <form>
-        <input type="text" name="pokemon" id="pokemon">
-        <button type="submit">Consultar</button>
-        <button type="submit">Consultar</button> 
-        <button id="buttonConsulta" type="button">Consultar a PHP</button>
+    <h1>CONSULTA CON PHP</h1>
+    <h3>ATRAVEZ DE APIFETCH</h3>
+    <form id="formu">
+        <input type="text" name="dato" id="dato" placeholder="Ingresa tu nombre">
+        <button id="enviar" >HAGA LA CONSULTA</button>
     </form>
+    <p id="resul"></p>
+
     <script src="java.js"></script>
 </body>
 </html>
